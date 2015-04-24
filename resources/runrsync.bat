5c3eae24efe0ba195f43b1110d5f52c598b42052
Sebastian Gun <sebastian.gun@mercadolibre.com>
2015-04-24 11:38:11 -0300

    Copy symbolic links as regular files

--- resources/runrsync.bat
+++ resources/runrsync.bat
@@ -3,7 +3,7 @@ START /MIN "PLINK_FOR_RSYNC" plink -i %3 -v -t -l %1 %2 sh /root/restartrsync
 REM Stop Resin
 START /MIN "PLINK_FOR_RSYNC" plink -i %3 -v -t -l %1 %2 sh /data1/resin/bin/httpd.sh stop
 REM Iterate through filenames in synclist.txt and rsync them
-for /F "tokens=1,2 delims=;" %%i in (%4) do rsync -avz --del %%i rsync://%1@%2:1873/%%j
+for /F "tokens=1,2 delims=;" %%i in (%4) do rsync -avzL --del %%i rsync://%1@%2:1873/%%j
 REM Sleep for 2 seconds to give rsync enough time to finish
 START /MIN /WAIT sleep 2
 REM Start Resin
